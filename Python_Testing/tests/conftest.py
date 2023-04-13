28a1b0cae73f4ad78df57595821545c8a3e56170
Tom du Merle <tomdumerle@macbook-pro-de-tom.home>
2023-04-13 08:34:24 +0200

    first commit résolution du bug overbooking

---
+++ Python_Testing/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+from server import app
+
+@pytest.fixture
+def created_app():
+
+    created_app = app
+    
+    yield created_app
+
+@pytest.fixture
+def client(created_app):
+    return created_app.test_client()
+
+@pytest.fixture
+def context(created_app):
+    return created_app.test_request_context()
+        
---
+++ Python_Testing/tests/unit/test_number_point_available.py
@@ -0,0 +1,21 @@
+from server import loadCompetitions
+
+
+def test_overbooking_not_possible(client):
+
+    competitions = loadCompetitions()
+    number_of_place = 8
+    
+    response_third = client.post('/purchasePlaces', data={ 'club' : 'Simply Lift', 'competition' : competitions[0]['name'], 'places' : number_of_place})
+
+    response_seconde_two = client.get('/book/Spring%20Festival/Simply%20Lift')
+
+    assert str(int(competitions[0]['numberOfPlaces'])-number_of_place) in response_seconde_two.data.decode()
+    assert (int(competitions[0]['numberOfPlaces'])-number_of_place) > 0
+
+
+
+
+
+   
+
